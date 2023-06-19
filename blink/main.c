1fd5352e7ce842db3bb598bb4138ed6ca011600b
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-18 20:18:20 -0700

    Add documentation url

--- blink/main.c
+++ blink/main.c
@@ -14,6 +14,20 @@ void vBlinkTask(){
 void main(){
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    /*
+        Documentation on xTaskCreate
+        https://www.freertos.org/a00125.html
+
+        
+        BaseType_t xTaskCreate(    TaskFunction_t pvTaskCode,
+                                   const char * const pcName,
+                                   configSTACK_DEPTH_TYPE usStackDepth,
+                                   void *pvParameters,
+                                   UBaseType_t uxPriority,
+                                   TaskHandle_t *pxCreatedTask
+                                 );
+    */
     xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
     vTaskStartScheduler();
 }
