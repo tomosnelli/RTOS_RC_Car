59cbfdb16d6a92728f3f484b3eec4ac95f19c22d
Tomo Kaneko <tomo.kaneko@proton.me>
2023-07-02 21:22:58 -0700

    Add working version of motor control with PWM

--- practice/motorSpeedControl/main.c
+++ practice/motorSpeedControl/main.c
@@ -12,8 +12,19 @@
 #define IN2_PIN 0
 
 #define PWM_RANGE 100
+#define SPEED_LOW 0.0
+#define SPEED_HIGH 1.0
+
+pwm_config config;
+uint pwm_slice_num;
+
+void default_pin_init(){
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+}
 
 void motor_init(){
+    // set GPIO pin
     gpio_init(IN1_PIN);
     gpio_init(IN2_PIN);
 
@@ -24,26 +35,23 @@ void motor_init(){
     gpio_put(IN2_PIN, 0);
 
     // configure ENA_PIN
-    /*
-    pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 25.0f); // Set 
-    pwm_config_set_wrap(&motor_ena_config, PWM_RANGE);
-    pwm_init(ENA_PIN, &motor_ena_config, true);
-    pwm_set_enabled(ENA_PIN, true);
-    pwm_set_gpio_level(ENA_PIN, true);
-    */
-
     gpio_set_function(ENA_PIN, GPIO_FUNC_PWM);
+    pwm_slice_num = pwm_gpio_to_slice_num(ENA_PIN);
 
-    uint slice_num = pwm_gpio_to_slice_num(ENA_PIN);
-    pwm_set_wrap(slice_num, PWM_RANGE);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
-    pwm_set_enabled(slice_num, true);
+    config = pwm_get_default_config();
+    pwm_init(pwm_slice_num, &config, true);
 }
 
-void motor_set_speed(int speed){
-    uint slice_num = pwm_gpio_to_slice_num(ENA_PIN);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, speed); // set PWM duty cycle (0-100%)
+void init_all(){
+    sleep_ms(3000);
+    stdio_init_all();
+    default_pin_init();
+    motor_init();  
+}
+
+void motor_set_speed(float speed){
+    if(SPEED_LOW <= speed && speed <= SPEED_HIGH)
+        pwm_set_gpio_level(ENA_PIN, (config.top * speed));
 }
 
 void forward(){
@@ -55,6 +63,7 @@ void forward(){
 void backward(){
     gpio_put(IN1_PIN, 0);
     gpio_put(IN2_PIN, 1);
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
 }
 
 void stop(){
@@ -65,24 +74,21 @@ void stop(){
 
 void vMotorTask(){
     for(;;){
+        motor_set_speed(0.20);
         forward();
         vTaskDelay(pdMS_TO_TICKS(1000));
         stop();
         vTaskDelay(pdMS_TO_TICKS(1000));
-        motor_set_speed(50);
+        motor_set_speed(0.90);
         forward();
         vTaskDelay(pdMS_TO_TICKS(1000));
         stop();
-        motor_set_speed(99);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void main(){
-    sleep_ms(3000);
-    stdio_init_all();
-    motor_init();
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    init_all();
 
     sleep_ms(250);
 
